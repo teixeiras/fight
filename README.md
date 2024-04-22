10bc6a16704c28d849a43e96a0b5feff5835b133
Filipe Teixeira <763604+teixeiras@users.noreply.github.com>
2024-04-22 12:24:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+What is expected:
+- Work with vscode as IDE
+
+
+aclocal
+autoconf
+autoheader
+automake --add-missing
+./configure 
+make
---
+++ arena/src/main.cpp
@@ -0,0 +1,13 @@
+  #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <string>
+
+  std::string libext = "so";
+
+
+int main(int argc, char** argv) {
+  printf("hello world!\n");
+  return 0;
+}
